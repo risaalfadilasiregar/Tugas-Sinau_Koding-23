54a62ec220191a926196484c265a54a0c8b538fd
Risa Alfadila <85499965+risaalfadilasiregar@users.noreply.github.com>
2022-10-24 13:46:55 +0700

    Add files via upload

---
+++ Latihan1/Latihan1.java
@@ -0,0 +1,13 @@
+package Latihan1;
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        for(int a = 1; a<=10;a++){
+            if (a%2 == 0){
+                System.out.println("Angka " + a + " ini adalah Genap");
+            }else{
+                System.out.println("Angka " + a + " Ini adalah Ganjil");
+            }
+        }
+    }
+}
